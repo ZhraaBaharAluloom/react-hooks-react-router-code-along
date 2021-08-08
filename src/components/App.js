e4e16c3b95d781fa3ea6cdab11f9ae1ddb63af7e
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-08 12:47:48 -0700

    add react router dom

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+// Components
+import About from "./About";
+import Login from "./Login";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Add NavLink to import */
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+  document.getElementById("root")
+);
